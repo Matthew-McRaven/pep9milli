a92d3493b4e69da4e7d58e72fc383465136444d6
Matthew McRaven <mkm302@georgetown.edu>
2019-10-11 21:02:52 -0400

    Looping example.
    Create a program that loops until a (simple) condition is fulfilled.

--- dummymain.c
+++ dummymain.c
@@ -4,53 +4,37 @@
 #include "alufunc.h"
 #include "klee/klee.h"
 
+WORD shiftin1(WORD a) {
+    return add_nocarry(asl(a),1);
+}
 int main(int argv, char** argc)
 {
     // Declare symbolic registers
-    WORD regA, regB, regC, regD;
+    WORD regA, regB, regC;
     klee_make_symbolic(&regA, sizeof(regA), "A");
     klee_make_symbolic(&regB, sizeof(regB), "B");
     klee_make_symbolic(&regC, sizeof(regC), "C");
-    klee_make_symbolic(&regD, sizeof(regD), "D");
 
     // Assume that no registers start being equal.
-    klee_assume(regA != regB); klee_assume(regA != regC); klee_assume(regA != regD);
-    klee_assume(regB != regC); klee_assume(regB != regD);
-    klee_assume(regC != regD);
+    klee_assume(regA != regB); klee_assume(regA != regC);
+    klee_assume(regB != regC);
     
-    // Create two temporary computation registers.
-    WORD t1 = xor(regC, regB);
-    WORD t2;
-    klee_make_symbolic(&t1, sizeof(t1), "t1");
-    klee_make_symbolic(&t2, sizeof(t2), "t2");
-
-    // Branch depnding on the value of t1.
-    if(t1 % 2) {
-        t2 = add_nocarry(t1, regA);
-    }
-    else {
-        t2 = add_nocarry(~t1, ~regA);
-    }
-
-    // Branch depnding on the value of t2.
-    if(t2 % 2) {
-        regA = or(t2, regD);
-    }
-    else {
-        regA = or(t2, regD);
-        regA = not(regA);
+    // Force B to always be positive.
+    WORD (*func_arr[2])(WORD);
+    func_arr[0] = &asr;
+    func_arr[1] = &shiftin1;
+    int which = 0;
+    while(regA != 0x11) {
+        regA = (*func_arr[which])(regA);
+        if((regA == 0) | (regA == 1)) {
+            which = 1;
+        }
+        else {
+            which = 0;
+        }
     }
 
-    // Branch depnding on the value of a.
-    if(regA % 2) {
-        regB = add_nocarry(regA, 1);
-    }
-    else {
-        regB = or(regA, 1);
-    }
-    
-    // Assert that no two paths allow A and B to be equal.
-    klee_assert(ident(regA) != ident(regB));
-    printf("Hello world!\n");
+
+    //printf("Hello world!\n");
     return 0;
 }
