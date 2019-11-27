ff977708d2f2fef379293dbf856bb873b12a41f9
Matthew McRaven <mkm302@georgetown.edu>
2019-11-26 19:01:02 -0500

    Fix shift and add program
    My earliest attempt at verification was a program that did the following.
    
    It would take the contents of a register, and decrement the register until it was zero.
    Then, it would set the register to 1.
    Then, it would shift the register right until it equalled 8.
    Then, it would terminate.
    
    This program demonstrates self looping and has a large state space (256 possible paths), so it made a good sample program for Klee.
    
    This program has been updated to use the most modern versions of my verification model.

--- simmain/simbytetest.c
+++ simmain/simbytetest.c
@@ -1,41 +1,64 @@
-#include "simbytetest.h"
+#include "sim.h"
+
 #include "alufunc.h"
 #include "cpu.h"
-#include <stdlib.h>
 #include "klee/klee.h"
 
+#include <stdlib.h>
 
-void initMCArray()
-{
-    /*microcodeTable = malloc(size*sizeof(MicrocodeLine));
-    klee_make_symbolic(microcodeTable, size*sizeof(MicrocodeLine), "ml");
-    microcodeTable[0] = &suba1;
-    microcodeTable[1] = &seta1;
-    microcodeTable[2] = &asla1;
-    microcodeTable[3] = &stop;*/
-}
+FLAG checkNeg(struct VerificationModel* model);
+FLAG invIfNeg(struct VerificationModel* model);
+FLAG suba1 (struct VerificationModel* model);
+FLAG seta1 (struct VerificationModel* model);
+FLAG asla1 (struct VerificationModel* model);
+FLAG stop (struct VerificationModel* model);
 
-void initCPU(struct CPU *cpu)
+
+static MicrocodeLine microcodeTable[] = 
 {
+    checkNeg, //0
+    invIfNeg, //1
+    suba1, //2
+    seta1, //3
+    asla1, //4
+    stop //5
+};
+
+void init_model(struct VerificationModel* model)
+{
+    // Cache pointer to CPU to reduce code verbosity.
+    struct CPU* cpu = model->cpu;
+
     //zeroCPU(cpu);
     cpu->microPC = 0;
 }
 
-FLAG testCPU(struct CPU *cpu)
+FLAG test_model(struct VerificationModel* model)
 {
+    // Cache pointer to CPU to reduce code verbosity.
+    struct CPU* cpu = model->cpu;
+
     FLAG result = 1;
     //printf("%d", cpu->regBank.registers[0]);
     klee_assert(cpu->regBank.registers[0] == 8);
     return 1;
 }
 
-FLAG stop (struct CPU* cpu)
+MicrocodeLine fetch_current_line(struct CPU *cpu)
+{
+    return microcodeTable[cpu->microPC];
+}
+
+FLAG stop (struct VerificationModel* model)
 {
     return 1;
 }
 
-FLAG checkNeg(struct CPU* cpu)
+FLAG checkNeg(struct VerificationModel* model)
 {
+    // Cache pointer to CPU to reduce code verbosity.
+    struct CPU* cpu = model->cpu;
+
     if(byte_ident(cpu->regBank.registers[0]).NZVC[N]){
         //klee_assert(cpu->regBank.registers[0] >= 128);
         cpu->microPC = 1;
@@ -46,8 +69,11 @@ FLAG checkNeg(struct CPU* cpu)
     }
     return 0;
 }
-FLAG invIfNeg(struct CPU* cpu)
+FLAG invIfNeg(struct VerificationModel* model)
 {
+    // Cache pointer to CPU to reduce code verbosity.
+    struct CPU* cpu = model->cpu;
+
     BYTE* target = &(cpu->regBank.registers[0]);
     //klee_assert(cpu->regBank.registers[0] >= 128);
     *target =  byte_and(*target, 127).result;
@@ -56,8 +82,11 @@ FLAG invIfNeg(struct CPU* cpu)
     return 0;
 }
 
-FLAG suba1 (struct CPU* cpu)
+FLAG suba1 (struct VerificationModel* model)
 {
+    // Cache pointer to CPU to reduce code verbosity.
+    struct CPU* cpu = model->cpu;
+
     BYTE* target = &(cpu->regBank.registers[0]);
     //klee_assert(cpu->regBank.registers[0] <= 127);
     *target =  byte_sub_nocarry(*target,1).result;
@@ -71,16 +100,22 @@ FLAG suba1 (struct CPU* cpu)
     }
     return 0;
 }
-FLAG seta1 (struct CPU* cpu)
+FLAG seta1 (struct VerificationModel* model)
 {
+    // Cache pointer to CPU to reduce code verbosity.
+    struct CPU* cpu = model->cpu;
+
     cpu->regBank.registers[0] =  byte_add_nocarry(cpu->regBank.registers[0], 1).result;
     //klee_assert(cpu->regBank.registers[0] == 1);
     cpu->microPC = 4;
     return 0;
 }
 
-FLAG asla1 (struct CPU* cpu)
+FLAG asla1 (struct VerificationModel* model)
 {
+    // Cache pointer to CPU to reduce code verbosity.
+    struct CPU* cpu = model->cpu;
+
     //klee_assert(cpu->regBank.registers[0] < 8);
     cpu->regBank.registers[0] =  byte_asl(cpu->regBank.registers[0]).result;
 
