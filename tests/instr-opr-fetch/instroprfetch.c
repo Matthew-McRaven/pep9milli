2ed40351c6c3b3523b2c83206a0625d71ebe2d23
Matthew McRaven <mkm302@georgetown.edu>
2019-12-02 18:12:09 -0500

    Implement IsUnary decoder.
    Assign boolean values to IsUnary decoder based on the Pep/9 RTL specification.

--- tests/instr-opr-fetch/instroprfetch.c
+++ tests/instr-opr-fetch/instroprfetch.c
@@ -124,12 +124,14 @@ void init_model(struct VerificationModel *model)
     if(cpu->PSNVCbits[P]) {
         cpu->regBank.registers[11] = IS_Value;
     }
+    // Default assume all instructions are unary.
+    memset(is_unary_decoder, 0, 256);
+    // Instructions from 0 to 17 are unary.
+    // Instructions from 18-37 are non-unary.
+    for(int it=18; it <= 37; it++) is_unary_decoder[it] = 1;
+    // Instructions from 38 to 79 are unary.
+    for(int it=80; it <= 255; it++) is_unary_decoder[it] = 1;
 
-    // Assume every instruction is unary
-    /*for(int it=0; it <= 255; it++) {
-        is_unary_decoder[it] =
-    }*/
-    //memset(is_unary_decoder, 0, 256);
 }
 
 FLAG test_model(struct VerificationModel *model)
